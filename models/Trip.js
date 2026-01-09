6a9bb3f8fb0b3fbde64f5334c4060c77ca4e3500
Simphiwe396 <djangojuice863@gmail.com>
2026-01-10 00:32:47 +0200

    Add Trip schema for MongoDB using Mongoose

--- models/Trip.js
+++ models/Trip.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
+
+const TripSchema = new mongoose.Schema({
+  pickupLat: Number,
+  pickupLng: Number,
+  dropLat: Number,
+  dropLng: Number,
+  lat: Number,
+  lng: Number,
+  distance: Number,
+  fare: Number,
+  rate: Number,
+  status: { type: String, default: "active" }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Trip", TripSchema);
 
